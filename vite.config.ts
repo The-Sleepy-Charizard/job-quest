48e71383e0c1b144d8227c71ca872233c1359d6c
jwu8475 <jwu8475@gmail.com>
2023-11-20 13:33:22 -0600

    added tailwind, react router

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/user': {
+        target: 'http://localhost:3030',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    port: 5173,
+  },
 })
