002c8ac1bf5e5263d54c44a6dad24a9ee8fc0707
Garrett <Garrett.byrne12@gmail.com>
2023-11-21 18:34:41 -0500

    corrected filter fetch

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,29 +1,29 @@
-import useAuthenticate from "../Hooks/useAuthenticate.tsx";
-import Navbar from '../components/Navbar.tsx'
-import JobContainer from '../components/JobContainer.tsx'
+import useAuthenticate from '../Hooks/useAuthenticate.tsx';
+import Navbar from '../components/Navbar.tsx';
+import JobContainer from '../components/JobContainer.tsx';
 import { useState, useEffect } from 'react';
 import { DashProp } from '../../types.ts';
 
 const Dashboard = ({ username }: DashProp) => {
-  const [ filter, setFilter ] = useState('bookmarked');
-  const [ jobs, setJobs ] = useState([]);
+  const [filter, setFilter] = useState('bookmarked');
+  const [jobs, setJobs] = useState([]);
   // fetch jobs when Navbar filter has changed
   useEffect(() => {
-    const endpoint = `/jobs/username=${username}&filter=${filter}`;
+    const endpoint = `/jobs?username=${username}&filter=${filter}`;
     const fetchData = async () => {
       const response = await fetch(endpoint);
       const newJobs = await response.json();
       setJobs(newJobs);
-    }
-    fetchData()
-  },[filter])
+    };
+    fetchData();
+  }, [filter]);
   useAuthenticate();
   return (
     <div>
       <Navbar setFilter={setFilter} />
-      <JobContainer jobs={jobs}/>
+      <JobContainer jobs={jobs} />
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
